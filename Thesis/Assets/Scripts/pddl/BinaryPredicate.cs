67839d08a9df05d63e03b0afa9b58617a020ab66
Michelangelo Diamanti <michelangelo.diamanti@gmail.com>
2018-04-23 18:27:52 +0000

    initial PDDL

---
+++ Thesis/Assets/Scripts/pddl/BinaryPredicate.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BinaryPredicate : IPredicate {
+
+	private EntityType _source;
+	private EntityType _destination;
+	private string _name;
+
+	public string Name
+	{
+		get { return _name; }
+	}
+
+	public EntityType Source
+	{
+		get { return _source; }
+	}
+	public EntityType Destination
+	{
+		get { return _destination; }
+	}
+
+	public BinaryPredicate(EntityType source, string name, EntityType destination){
+		if(source == null)
+			throw new System.ArgumentException("Predicate source type cannot be null", "source type");
+		if(name == null)
+			throw new System.ArgumentException("Predicate name cannot be null", "name");
+		if(destination == null)
+			throw new System.ArgumentException("Predicate destination type cannot be null", "destination type");
+
+		if(Manager.GetManager().predicateExists(source, name, destination))
+			throw new System.ArgumentException("Predicate has already been declared", name);
+
+		_source = source;
+		_name = name;
+		_destination = destination;
+	}
+
+	public override string ToString(){
+		return _source + " " + _name + " " + _destination; 
+	}
+
+}
---
+++ Thesis/Assets/Scripts/pddl/InitialWorld.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Data;
+
+public class InitialWorld : MonoBehaviour {
+
+	DataBaseAccess db;
+	List<Entity> entities;
+	Manager man;
+
+    // Use this for initialization
+    void Start () {
+
+		man = Manager.GetManager();
+
+		EntityType character = new EntityType("CHARACTER");
+		EntityType location = new EntityType("LOCATION");
+
+		man.addEntityType(character);
+		man.addEntityType(location);
+
+		Entity john = new Entity(character, "john");
+		Entity school = new Entity(location, "school");
+
+		man.addEntity(john);
+		man.addEntity(school);
+
+		BinaryPredicate isAt = new BinaryPredicate(character, "IS_AT", location);
+		// BinaryPredicate isAt2 = new BinaryPredicate(character, "IS_AT", location);
+		UnaryPredicate isRich = new UnaryPredicate(character, "RICH");
+
+		man.addPredicate(isAt);
+		man.addPredicate(isRich);
+
+		Debug.Log("predicates count: " + man.getPredicates().Count);
+
+		foreach(IPredicate p in man.getPredicates()){
+			if(p.GetType() == typeof(BinaryPredicate))
+				Debug.Log(p + " binary");
+			if(p.GetType() == typeof(UnaryPredicate))
+				Debug.Log(p + " unary");
+		}
+
+		// Manager.GetManager().addPredicate(new Predicate("IS_AT",2));
+		// foreach( Entity e in man.getEntities()){
+		// 	Debug.Log(e);
+		// }
+
+		UnaryRelation johnIsRich = new UnaryRelation(john, isRich);
+		BinaryRelation johnIsAtSchool = new BinaryRelation(john, isAt, school);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// private void parseEntities(){
+	// 	IDataReader res = db.BasicQuery("SELECT rowid, * FROM ENTITY", true);
+    //     while(res.Read()) { 
+	// 		long id = -1;
+	// 		string type = "";
+	// 		string name = "";
+	// 		bool real = false;
+	// 		int world = -1;
+    //         for (int i = 0; i < res.FieldCount; i++){
+    //             string field = res.GetName(i);
+	// 			switch(field){
+	// 				case "rowid":
+	// 					id = res.GetInt64(i);
+	// 					break;
+	// 				case "TYPE":
+	// 					type = res.GetString(i);
+	// 					break;
+	// 				case "NAME":
+	// 					name = res.GetString(i);
+	// 					break;
+	// 				case "REAL":
+	// 					real = res.GetBoolean(i);
+	// 					break;
+	// 				case "WORLD":
+	// 					world = res.GetInt32(i);
+	// 					break;
+	// 			}
+	// 		}
+	// 		entities.Add(new Entity(id, type, name, real, world));
+	// 	}
+
+	// 	foreach(Entity e in entities){
+	// 		Debug.Log(e.Type + " " + e.Name + " " + e.Real + " " + e.World);
+	// 	}
+	// 	Debug.Log(entities);	
+	// }
+}
