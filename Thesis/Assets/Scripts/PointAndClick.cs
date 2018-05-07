ccf24c8f21819c18513d0c996891c91b88b99404
Matteo Altobelli <matteo.altobelli.92@gmail.com>
2018-05-07 16:48:00 +0200

    Generalized VillageSceneManager

--- Thesis/Assets/Scripts/PointAndClick.cs
+++ Thesis/Assets/Scripts/PointAndClick.cs
@@ -1,8 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PointAndClick : MonoBehaviour {
+
+	public Object description;
+	private Object desc;
+	private string name;
 
 	// Use this for initialization
 	void Start () {
@@ -16,11 +21,43 @@ public class PointAndClick : MonoBehaviour {
             RaycastHit hitInfo = new RaycastHit();
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo) && hitInfo.transform.tag == "Building")
             {
+				name = hitInfo.transform.name;
                 print (hitInfo.transform.name);
-            }else
+				RetriveInformations();
+				ShowDescription();
+			}else
 			{
-				 print ("NOTHING");
+				DestroyDescription();
+				print ("NOTHING");
 			}
         }	
+	}
+
+	private void ShowDescription() {
+		
+		if(desc != null)
+		{
+			DestroyDescription();
+		}
+		Vector3 position = new Vector3(0,0,0);
+		desc = Instantiate(description, GameObject.Find("Canvas").transform, instantiateInWorldSpace:false);
+		Text title = GameObject.Find("Title").GetComponent<Text>();
+		title.text = name; 
+		// desc.transform.SetParent(GameObject.Find("Canvas").transform);
+		// RectTransform descriptionTansform =  description.GetComponent<RectTransform>();
+		// descriptionTansform.anchorMin = Input.mousePosition;
+	
+	}
+
+	private void DestroyDescription() {
+
+		Destroy(desc);
+
+	}
+
+	public void RetriveInformations(){
+
+		//implement a method to collect the current state's informations.
+
 	}
 }
--- Thesis/Assets/Scripts/VillageSceneManager.cs
+++ Thesis/Assets/Scripts/VillageSceneManager.cs
@@ -3,43 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class VillageSceneManager : MonoBehaviour {
-	public string name;
-	public bool flag; 
-	GameObject Market1;
-	GameObject Market2;
-	GameObject Church1;
+	public string inputName;
+	public bool flag;
 
-	GameObject Church2;
-
-	GameObject Inn1;
-
-	GameObject Inn2;
-	List<GameObject> list = new List<GameObject>();
+	// GameObject Market1;
+	// GameObject Market2;
+	// GameObject Church1;
+	// GameObject Church2;
+	// GameObject Inn1;
+	// GameObject Inn2;
+	// List<GameObject> list = new List<GameObject>();
 	
 
 	// Use this for initialization
 	void Start () {
 		flag = false;
 
-		Market1 = GameObject.Find("Market1PS");
-		list.Add(Market1);
-		Market1.SetActive(false);
-		Church1 = GameObject.Find("Church1PS");
-		list.Add(Church1);
-		Church1.SetActive(false);
-		Inn1 = GameObject.Find("Inn1PS");
-		list.Add(Inn1);
-		Inn1.SetActive(false);
+		foreach (ParticleSystem ps in Resources.FindObjectsOfTypeAll(typeof(ParticleSystem)) as ParticleSystem[])
+		{
+			ParticleSystem.EmissionModule emission = ps.emission;
+			emission.enabled = false;
+		}
+		// Market1 = GameObject.Find("Market1PS");
+		// list.Add(Market1);
+		// Market1.SetActive(false);
+		// Church1 = GameObject.Find("Church1PS");
+		// list.Add(Church1);
+		// Church1.SetActive(false);
+		// Inn1 = GameObject.Find("Inn1PS");
+		// list.Add(Inn1);
+		// Inn1.SetActive(false);
 
-		Market2 = GameObject.Find("Market2PS");
-		list.Add(Market2);
-		Market2.SetActive(false);
-		Church2 = GameObject.Find("Church2PS");
-		list.Add(Church2);
-		Church2.SetActive(false);
-		Inn2 = GameObject.Find("Inn2PS");
-		list.Add(Inn2);
-		Inn2.SetActive(false);
+		// Market2 = GameObject.Find("Market2PS");
+		// list.Add(Market2);
+		// Market2.SetActive(false);
+		// Church2 = GameObject.Find("Church2PS");
+		// list.Add(Church2);
+		// Church2.SetActive(false);
+		// Inn2 = GameObject.Find("Inn2PS");
+		// list.Add(Inn2);
+		// Inn2.SetActive(false);
 	}
 	
 	// Update is called once per frame
@@ -48,57 +51,72 @@ public class VillageSceneManager : MonoBehaviour {
 		if(flag)
 		{
 			// Calls the function Manager with the string typed in the public variable "name"
-    		gameObject.SendMessage("Manager", name);
+			// Every script attached to the game object that has an Manager function will be called.
+    		gameObject.SendMessage("Manager", inputName);
 			flag = false;
 		}
 		
 	}
 
-	// Every script attached to the game object
-    // that has an Manager function will be called.
     public void Manager (string s) {
 
         print (s);
-
-		if (s == "Market1")
-			{
-				Market1.SetActive(true);
-			}
-			else if(s == "Market2")
-			{
-				Market2.SetActive(true);
-			}
-			else if(s == "Church1")
-			{
-				Church1.SetActive(true);
-			}
-			else if(s == "Church2")
-			{
-				Church2.SetActive(true);
-			}
-			else if(s == "Inn1")
-			{
-				Inn1.SetActive(true);
-			}
-			else if(s == "Inn2")
-			{
-				Inn2.SetActive(true);
-			}
 		
-		foreach(GameObject x in list)
-		{
-			if(x.activeSelf && (x.name != s + "PS"))
-			{
-				x.SetActive(false);
-			}
-		}
-
 		//Resources.FindObjectsOfTypeAll finds all the object of the 
 		//specified type, even if they are inactive
 		//
+		foreach (ParticleSystem ps in Resources.FindObjectsOfTypeAll(typeof(ParticleSystem)) as ParticleSystem[])
+		{
+			if(ps.name == inputName + "PS"){
+				ParticleSystem.EmissionModule emission = ps.emission;
+				emission.enabled = true;
+			}else{
+				if(ps.emission.enabled == true){
+					ParticleSystem.EmissionModule emission = ps.emission;
+					emission.enabled = false;
+				}
+			}
+		}
+		
 		// foreach (GameObject x in Resources.FindObjectsOfTypeAll(typeof(GameObject)) as GameObject[])
 		// {
-			
+		// 	if(x.name == inputName){
+		// 		GameObject particleEffect = x.transform.Find(inputName + "PS").gameObject;
+		// 		particleEffect.SetActive(true);
+		// 	}
+		// }
+
+		// if (s == "Market1")
+		// 	{
+		// 		Market1.SetActive(true);
+		// 	}
+		// 	else if(s == "Market2")
+		// 	{
+		// 		Market2.SetActive(true);
+		// 	}
+		// 	else if(s == "Church1")
+		// 	{
+		// 		Church1.SetActive(true);
+		// 	}
+		// 	else if(s == "Church2")
+		// 	{
+		// 		Church2.SetActive(true);
+		// 	}
+		// 	else if(s == "Inn1")
+		// 	{
+		// 		Inn1.SetActive(true);
+		// 	}
+		// 	else if(s == "Inn2")
+		// 	{
+		// 		Inn2.SetActive(true);
+		// 	}
+		
+		// foreach(GameObject x in list)
+		// {
+		// 	if(x.activeSelf && (x.name != s + "PS"))
+		// 	{
+		// 		x.SetActive(false);
+		// 	}
 		// }
 		
 	}
