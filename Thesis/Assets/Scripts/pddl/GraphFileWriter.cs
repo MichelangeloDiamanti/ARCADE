08f92d559385e3bbc9034aa017d4d778fc4f9fd6
Giulio <mori.giulio.94@gmail.com>
2018-06-06 15:47:47 +0200

    starting graph generation

---
+++ Thesis/Assets/Scripts/pddl/GraphFileWriter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class GraphFileWriter : MonoBehaviour
+{
+    public void SaveFile()
+    {
+        string destination = Application.persistentDataPath + "/example1.graphml";
+        FileStream file;
+
+        if (File.Exists(destination)) file = File.OpenWrite(destination);
+        else file = File.Create(destination);
+
+        // GameData data = new GameData(currentScore, currentName, currentTimePlayed);
+		string data = "";
+        BinaryFormatter bf = new BinaryFormatter();
+        bf.Serialize(file, data);
+        file.Close();
+    }
+
+    public void LoadFile()
+    {
+        string destination = Application.persistentDataPath + "/example1.graphml";
+        FileStream file;
+
+        if (File.Exists(destination)) file = File.OpenRead(destination);
+        else
+        {
+            Debug.LogError("File not found");
+            return;
+        }
+
+        BinaryFormatter bf = new BinaryFormatter();
+        // GameData data = (GameData)bf.Deserialize(file);
+        file.Close();
+    }
+}
