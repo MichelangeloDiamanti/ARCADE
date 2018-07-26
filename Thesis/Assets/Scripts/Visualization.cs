81f1bd1cdcda5596b26ab6aebd75b6f587a07dfb
M4tt92 <matteo.altobelli.92@gmail.com>
2018-07-26 16:34:06 +0200

    Visualization\Action class added

--- Thesis/Assets/Scripts/Visualization.cs
+++ Thesis/Assets/Scripts/Visualization.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using ru.cadia.pddlFramework;
+using vis = ru.cadia.visualization;
+
 public class Visualization : MonoBehaviour
 {
 
@@ -32,7 +34,7 @@ public class Visualization : MonoBehaviour
 
     }
 
-    public IEnumerator interact(Action a, System.Action<bool> result)
+    public IEnumerator interact(vis.Action a, System.Action<bool> result)
     {
         displayText.text = "The Simulator is requesting the following Action: " + a.ShortToString();
 
@@ -55,13 +57,80 @@ public class Visualization : MonoBehaviour
         }
     }
 
-    public IEnumerator visualize(Action a, System.Action<bool> result)
+    public IEnumerator visualize(vis.Action a, System.Action<bool> result)
     {
         displayText.text = "The Simulator is requesting the following Action: " + a.ShortToString();
 
         yield return new WaitForSeconds(visualizationWaitTime);
 
         // TODO: the logic to visualize the action should go here
+        //
+
+        print(a.Name);
+
+        HashSet<IRelation> preconditions = a.PreConditions;
+        HashSet<Entity> parameters = a.Parameters;
+        HashSet<IRelation> postconditions = a.PostConditions;
+
+        string preText = null;
+        string postText = null;
+        string actionText = null;
+        string destination = null;
+
+        //PREconditions
+        foreach (IRelation pre in preconditions)
+        {
+            if (pre.Value == RelationValue.TRUE)
+            {
+                if (pre.GetType() == typeof(vis.BinaryRelation))
+                {
+                    vis.BinaryRelation r = pre as vis.BinaryRelation;
+                    vis.BinaryPredicate p = pre.Predicate as vis.BinaryPredicate;
+                    preText += "the " + r.Source.Name + " " + p.Text + " " + r.Destination.Name + "\n";
+                }
+                else
+                {
+                    vis.UnaryRelation r = pre as vis.UnaryRelation;
+                    vis.UnaryPredicate p = pre.Predicate as vis.UnaryPredicate;
+                    preText += "the " + r.Source.Name + " " + p.Text + "\n";
+                }
+            }
+        }
+
+        //POSTconditions
+        foreach (IRelation post in postconditions)
+        {
+            if (post.Value == RelationValue.TRUE)
+            {
+                if (post.GetType() == typeof(vis.BinaryRelation))
+                {
+                    vis.BinaryRelation r = post as vis.BinaryRelation;
+                    vis.BinaryPredicate p = post.Predicate as vis.BinaryPredicate;
+                    postText += "the " + r.Source.Name + " " + p.Text + " " + r.Destination.Name + "\n";
+                }
+                else
+                {
+                    vis.UnaryRelation r = post as vis.UnaryRelation;
+                    vis.BinaryPredicate p = post.Predicate as vis.BinaryPredicate;
+                    preText += "the " + r.Source.Name + " " + p.Text + "\n";
+                }
+            }
+        }
+        // TO FIX
+        //
+        ////Action
+        //
+        //foreach (Entity param in parameters)
+        //{
+        //    if (param.Type.Equals(domain.getEntityType("CHARACTER")))
+        //    {
+        //        actionText += "the " + param.Name + " decided to " + act.Name + " to " + destination + "\n";
+        //    }
+        //}
+
+        displayText.text += "Initially " + preText + "\nThen " + actionText + "\nNow " + postText;
+
+        //
 
         float outcome = Random.Range(0.0f, 1.0f);
         if (outcome <= visualizationSuccessProbability)
---
+++ Thesis/Assets/Scripts/Visualization/Action.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ru.cadia.pddlFramework;
+using UnityEngine;
+
+namespace ru.cadia.visualization
+{
+    public class Action : ru.cadia.pddlFramework.Action
+    {
+        private HashSet<IRelation> _preConditions;
+        private string _name;
+        private HashSet<Entity> _parameters;
+        private HashSet<IRelation> _postConditions;
+        private string _text;
+
+        public HashSet<IRelation> PreConditions
+        {
+            get { return _preConditions; }
+        }
+        public string Name
+        {
+            get { return _name; }
+        }
+        public HashSet<Entity> Parameters
+        {
+            get { return _parameters; }
+        }
+        public HashSet<IRelation> PostConditions
+        {
+            get { return _postConditions; }
+        }
+        public string Text
+        {
+            get { return _text; }
+        }
+
+        public Action(HashSet<IRelation> preconditions, string name, HashSet<Entity> parameters, HashSet<IRelation> postconditions, string text) : base(preconditions, name, parameters, postconditions)
+        {
+            if (text == null)
+                throw new System.ArgumentNullException("ActionDefinition: The set of parameter cannot be null or empty", "HashSet<EntityType> parameter");
+
+            _text = text;
+        }
+
+        public Action sobstituteEntityInAction(Dictionary<Entity, Entity> sobstitutions)
+        {
+            Action newAction = null;
+            HashSet<IRelation> newPreConditions = sobstituteRoutine(sobstitutions, _preConditions);
+            HashSet<IRelation> newPostConditions = sobstituteRoutine(sobstitutions, _postConditions);
+            HashSet<Entity> entitiesInvolved = new HashSet<Entity>();
+            foreach (Entity item in sobstitutions.Values)
+            {
+                entitiesInvolved.Add(item);
+            }
+            newAction = new Action(newPreConditions, _name, entitiesInvolved, newPostConditions, _text);
+            return newAction;
+        }
+        private HashSet<IRelation> sobstituteRoutine(Dictionary<Entity, Entity> sobstitutions, HashSet<IRelation> condictions)
+        {
+            HashSet<IRelation> newConditions = new HashSet<IRelation>();
+            foreach (IRelation item in condictions)
+            {
+                if (item.GetType() == typeof(UnaryRelation))
+                {
+                    UnaryRelation ur = item as UnaryRelation;
+                    Entity source;
+                    if (sobstitutions.TryGetValue(ur.Source, out source))
+                    {
+                        newConditions.Add(new UnaryRelation(source, ur.Predicate, ur.Value));
+                    }
+                }
+                else if (item.GetType() == typeof(BinaryRelation))
+                {
+                    BinaryRelation br = item as BinaryRelation;
+                    Entity source, destination;
+                    if (sobstitutions.TryGetValue(br.Source, out source) && sobstitutions.TryGetValue(br.Destination, out destination))
+                    {
+                        newConditions.Add(new BinaryRelation(source, br.Predicate, destination, br.Value));
+                    }
+                }
+            }
+            return newConditions;
+        }
+
+        public Action Clone()
+        {
+            HashSet<IRelation> newPreConditions = new HashSet<IRelation>();
+            HashSet<Entity> newParameters = new HashSet<Entity>();
+            HashSet<IRelation> newPostConditions = new HashSet<IRelation>();
+
+            foreach (IRelation precondition in _preConditions)
+                newPreConditions.Add(precondition.Clone());
+            foreach (Entity e in _parameters)
+                newParameters.Add(e.Clone());
+            foreach (IRelation postcondition in _postConditions)
+                newPostConditions.Add(postcondition.Clone());
+
+            return new Action(newPreConditions, _name, newParameters, newPostConditions, _text);
+        }
+
+        public bool Equals(Action other)
+        {
+            if (other == null)
+                return false;
+
+            if (_name.Equals(other.Name) == false)
+                return false;
+
+            return true;
+        }
+
+        public string ShortToString()
+        {
+            string value = "Action: " + _name + "(";
+            foreach (Entity item in _parameters)
+            {
+                value += item.Name + " ,";
+            }
+            value += ")";
+            return value;
+        }
+
+    }
+}
+
